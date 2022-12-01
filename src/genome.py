d2e11d3d4d8668b408c840f88abec348fe984cf1
Aneta Ambrosova <ambroan1@fel.cvut.cz>
2022-12-01 21:34:32 +0100

    commit insert_te

--- src/genome.py
+++ src/genome.py
@@ -88,86 +88,76 @@ class Genome(ABC):
 class ListGenome(Genome):
     """
     Representation of a genome.
-
     Implements the Genome interface using Python's built-in lists
     """
 
-    genome : list[str]
-    TE : dict[int,int]
-    TE_ID : int
-
     def __init__(self, n: int):
         """Create a new genome with length n."""
-        self.genome = ['-']*n #create genome
-        self.TE = {} 
-        self.TE_ID = 1
+        #initialize the genome with no TE's yet.
+        self.nucleotide= ['-'] * n
+        self.TE = {}
+        self.active_TE = []
 
     def insert_te(self, pos: int, length: int) -> int:
         """
         Insert a new transposable element.
-
         Insert a new transposable element at position pos and len
         nucleotide forward.
-
         If the TE collides with an existing TE, i.e. genome[pos]
         already contains TEs, then that TE should be disabled and
         removed from the set of active TEs.
-
         Returns a new ID for the transposable element.
         """
-        
-        if self.genome[pos] in self.TE:
-            del self.TE[self.genome[pos]]
-        TE_ID = self.TE_ID
-        self.TE_ID = self.TE_ID + 1
+
+        if len(self.TE) != 0:
+            TE_ID = max(self.TE) + 1
+        else:
+            TE_ID = 1
         self.TE[TE_ID] = length
-        self.genome[pos] = [TE_ID] * length
+        self.active_TE.append(TE_ID)
+        if self.nucleotide[pos] in self.active_TE:
+            self.disable_te(self.nucleotide[pos])
+        self.nucleotide[pos:pos] = [TE_ID]*length
         return TE_ID
 
 
     def copy_te(self, te: int, offset: int) -> int | None:
         """
         Copy a transposable element.
-
         Copy the transposable element te to an offset from its current
         location.
-
         The offset can be positive or negative; if positive the te is copied
         upwards and if negative it is copied downwards. If the offset moves
         the copy left of index 0 or right of the largest index, it should
         wrap around, since the genome is circular.
-
         If te is not active, return None (and do not copy it).
         """
         return -1
+            
 
     def disable_te(self, te: int) -> None:
         """
         Disable a TE.
-
         If te is an active TE, then make it inactive. Inactive
         TEs are already inactive, so there is no need to do anything
         for those.
         """
         return -1
 
-
     def active_tes(self) -> list[int]:
         """Get the active TE IDs."""
-        return -1
+        return self.active_TE
 
     def __len__(self) -> int:
         """Current length of the genome."""
-        return -1
+        return len(self.nucleotide)
 
     def __str__(self) -> str:
         """
         Return a string representation of the genome.
-
         Create a string that represents the genome. By nature, it will be
         linear, but imagine that the last character is immidiatetly followed
         by the first.
-
         The genome should start at position 0. Locations with no TE should be
         represented with the character '-', active TEs with 'A', and disabled
         TEs with 'x'.
